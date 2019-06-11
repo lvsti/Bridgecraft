46a3b374edf696ff1f1b1943936830967294119c
Tamas Lustyik <elveestei@gmail.com>
2019-06-11 21:33:12 +0200

    Bumped version

--- Bridgecraft/main.swift
+++ Bridgecraft/main.swift
@@ -9,7 +9,7 @@
 import Commander
 import Foundation
 
-let version = "0.4.1"
+let version = "0.5.0"
 
 let generate = command(
     Flag("assume-nonnull", description: "assume that all headers have been audited for nullability"),
